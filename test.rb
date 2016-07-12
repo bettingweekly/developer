2d62ba9dd4dc6b32b6cce1a2a393e41b71d44e40
benfowlfx <ben.fowl@currencycloud.com>
2016-07-12 11:26:12 +0100

    Added method missing that returns self so that DataBase.new.find_developer does not fail

---
+++ test.rb
@@ -0,0 +1,28 @@
+class Developer
+  def method_missing(m, *args, &block)
+    return self
+  end
+end
+
+class DataBase
+  def find_developer(platform: :ruby, region: :london)
+    all_developers(platform, region).select do |you|
+      you.are.crazy
+          .and { your.skill_level is :high }
+          .and { you.are.not.in :plumbee }
+          .and { you.love 'ruby', 'rails' }
+          .and { want 'fun', 'money' }
+          .and.if you do
+        your work well
+      end
+    end
+  end
+
+  private
+
+  def all_developers(*_)
+    [Developer.new]
+  end
+end
+
+DataBase.new.find_developer
